5a37d6a194f30112938782b727b874d087801ff2
idemirli <ibrahimdemirli92@gmail.com>
2021-05-14 23:10:09 +0300

    Proje Oluşturuldu...

---
+++ CoreDbFirstLinQ/CoreDbFirstLinQ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDbFirstLinQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
